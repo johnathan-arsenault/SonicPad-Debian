11b769446c6222db4785d379fb9e427ed6db8cfb
johnathan-arsenault <15092354+johnathan-arsenault@users.noreply.github.com>
2025-09-26 11:01:41 -0400

    Update create_rootfs.sh

--- src/create_rootfs.sh
+++ src/create_rootfs.sh
@@ -57,7 +57,7 @@ start_spinner "Creating a basic rootfs"
     apt-get update
     apt-get install qemu-user-static -y
     apt-get install debootstrap=1.0.134ubuntu1 -y # Install only debootstrap, pi doesnt need it
-    debootstrap --no-check-gpg --foreign --verbose --arch=arm64 $DEB_DISTRO $ROOTFS_DIR $DEB_URL
+    debootstrap --no-check-gpg --no-merged-usr --foreign --verbose --arch=arm64 $DEB_DISTRO $ROOTFS_DIR $DEB_URL
     sed -i "s/$DEB_DISTRO main/$DEB_DISTRO main contrib/" $ROOTFS_DIR/etc/apt/sources.list
     cp /usr/bin/qemu-aarch64-static $ROOTFS_DIR/usr/bin/
     chmod +x $ROOTFS_DIR/usr/bin/qemu-aarch64-static
